a0441877ad68705fcc6a09799c73cbf7d8c46842
pankajchandgude <51829474+pankajchandgude@users.noreply.github.com>
2021-03-22 18:27:05 +0530

    3rd commit

--- SampleGit/src/main/Calculator.java
+++ SampleGit/src/main/Calculator.java
@@ -4,9 +4,12 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		add(10,20);
 	}
+	
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
-	  }
+	       
+	}
 
 }
