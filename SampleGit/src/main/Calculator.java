d8a70c4d06089ac4013a3e7253b0f8a0807a430a
pankajchandgude <51829474+pankajchandgude@users.noreply.github.com>
2021-03-23 09:50:03 +0530

    added mod func

--- SampleGit/src/main/Calculator.java
+++ SampleGit/src/main/Calculator.java
@@ -20,5 +20,9 @@ public class Calculator {
 	      System.out.println(num1*num2);
 	       
 	}
+	public static void mod(int num1,int num2){
+	      System.out.println(num1%num2);
+	       
+	}
 
 }
